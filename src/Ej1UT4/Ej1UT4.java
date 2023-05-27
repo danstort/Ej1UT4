4e1eba603731d15010a8be83c3a130bccdac0b33
danie <danie@DanielsLAPTOP>
2023-05-27 17:46:18 +0200

    Versión definitiva

---
+++ src/Ej1UT4/Ej1UT4.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ej1UT4;
+
+import java.util.Scanner;
+/**
+ *
+ * @author danie
+ */
+public class Ej1UT4 {
+
+    
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        int nota = 0;
+        nota = teclado.nextInt();
+        if(nota >= 5) {
+
+        System.out.println("APROBADO");
+        System.out.println("========");
+
+}
+        
+        System.out.println("========");
+    }
+    
+}
